5806e137825655e41c8691015146ba254dd7a700
abduqodirovshukrullo <abduqodirov0101@gmail.com>
2022-04-24 20:03:52 +0500

    Frontend pages created

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,49 @@
+import {useState, useEffect} from "react";
+import {FaSignInAlt} from "react-icons/fa";
+
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    })
+
+    const {email, password} = formData
+
+    const onNameChange = e => {
+        setFormData((prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        })))
+    }
+
+    const onSubmit = e => {
+        e.preventDefault()
+    }
+
     return (
-        <div>
-            Login
-        </div>
+        <>
+            <section className="heading">
+                <h1>
+                    <FaSignInAlt/> Login
+                </h1>
+                <p>Please enter your account</p>
+            </section>
+            <section className="form">
+                <form onSubmit={onSubmit}>
+                    <div className="form-group">
+                        <input className="input-control" type={'email'} id={'email'} name={'email'}
+                               placeholder={'Enter your email'} value={email} onChange={onNameChange}/>
+                    </div>
+                    <div className="form-group">
+                        <input className="input-control" type={'password'} id={'password'} name={'password'}
+                               placeholder={'Enter your password'} value={password} onChange={onNameChange}/>
+                    </div>
+                    <div className="from-control">
+                        <buttun type={'submit'} className="btn btn-block">Login</buttun>
+                    </div>
+                </form>
+            </section>
+        </>
     );
 };
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,59 @@
+import {useState, useEffect} from "react";
+import {FaUser} from "react-icons/fa";
+
 const Register = () => {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: "",
+    })
+
+    const {name, email, password, password2} = formData
+
+    const onNameChange = e => {
+        setFormData((prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        })))
+    }
+
+    const onSubmit = e => {
+        e.preventDefault()
+    }
+
     return (
-        <div>
-            Register
-        </div>
+        <>
+            <section className="heading">
+                <h1>
+                    <FaUser/> Register
+                </h1>
+                <p>Please create an account</p>
+            </section>
+            <section className="form">
+                <form onSubmit={onSubmit}>
+                    <div className="form-group">
+                        <input className="input-control" type={'text'} id={'name'} name={'name'}
+                               placeholder={'Enter your name'} value={name} onChange={onNameChange}/>
+                    </div>
+                    <div className="form-group">
+                        <input className="input-control" type={'email'} id={'email'} name={'email'}
+                               placeholder={'Enter your email'} value={email} onChange={onNameChange}/>
+                    </div>
+                    <div className="form-group">
+                        <input className="input-control" type={'password'} id={'password'} name={'password'}
+                               placeholder={'Enter your password'} value={password} onChange={onNameChange}/>
+                    </div>
+                    <div className="form-group">
+                        <input className="input-control" type={'password'} id={'password2'} name={'password2'}
+                               placeholder={'Confirm your password'} value={password2} onChange={onNameChange}/>
+                    </div>
+                    <div className="from-control">
+                        <buttun type={'submit'} className="btn btn-block">Submit</buttun>
+                    </div>
+                </form>
+            </section>
+        </>
     );
 };
 
